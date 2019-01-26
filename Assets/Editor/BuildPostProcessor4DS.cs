f6816b7cb3045adc55d3eeb90bfd6a4d24bfde21
Siba Siddique <siba@nyu.edu>
2019-01-26 16:23:01 +0100

    Upload files
    Ulpoad files to repo
    - implement text appear on object trigger

---
+++ Assets/Editor/BuildPostProcessor4DS.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+
+public class BuildPostProcessor4DS
+{
+
+#if UNITY_IOS
+
+    [PostProcessBuildAttribute(1)]
+	public static void OnPostProcessBuild(BuildTarget target, string path)
+	{
+		if (target == BuildTarget.iOS)
+		{
+			// Read.
+			string projectPath = PBXProject.GetPBXProjectPath(path);
+			PBXProject project = new PBXProject();
+			project.ReadFromString(File.ReadAllText(projectPath));
+			string targetName = PBXProject.GetUnityTargetName();
+			string targetGUID = project.TargetGuidByName(targetName);
+
+			AddFrameworks(project, targetGUID);
+
+			// Write.
+			File.WriteAllText(projectPath, project.WriteToString());
+		}
+	}
+
+	static void AddFrameworks(PBXProject project, string targetGUID)
+	{
+		project.AddFrameworkToProject(targetGUID, "libz.tbd", false);
+	}
+#endif
+}
---
+++ Assets/SCRIPTs/SphereController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereController : MonoBehaviour {
+	public Text textDesc;
+	public Vector3 lastPosition;
+	// Use this for initialization
+	void Start () {
+		lastPosition = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ZoomObj(){
+		 /*if (lastPosition.z != -8) {
+            lastPosition = new Vector3(lastPosition.x, lastPosition.y, lastPosition.z - 0.5f);
+            transform.position = lastPosition;
+        }*/
+        textDesc.gameObject.SetActive(true);
+	}
+
+}
